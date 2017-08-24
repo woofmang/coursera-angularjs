96d351972e5c802a5fd0298db00f395a0ddcc4ed
woofmang <kevin@woofmang.com>
2017-08-23 22:34:02 -0400

    lecture 9 example

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 (function(){
   'use strict';
-  angular.module('NameCalculator', [])
-  .controller('NameCalculatorController', function($scope){
-    $scope.name = '';
-    $scope.totalValue = 0;
-    $scope.displayTotalValue = function() {
-      var totalNameValue = calculateNumberForString($scope.name);
-      $scope.totalValue = totalNameValue;
+  angular.module('DIApp', [])
+  .controller('DIController', DIController);
+
+  function DIController($scope, $filter, $injector) {
+    $scope.name = "Kevin";
+    $scope.upper = function() {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
     };
-    function calculateNumberForString(string) {
-      var totalStringValue = 0;
-      for (var i = 0; i < string.length; i++) {
-        totalStringValue += string.charCodeAt(i);
-      }
-      return totalStringValue;
-    }
-  });
+    console.log($injector.annotate(DIController));
+  }
+
 })();
