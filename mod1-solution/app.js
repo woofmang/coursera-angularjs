138775dea6f13867c133c3868bf332038dc6ee45
woofmang <kevin@woofmang.com>
2017-08-27 08:56:54 -0400

    finish assignment 1

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -5,9 +5,38 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
+    $scope.lunchContents = "";
     $scope.lunchCheckResult = "";
     $scope.checkLunch = function() {
-      $scope.lunchCheckResult = "checked";
+      var result = "";
+      var number = checkIt();
+      if (number == 0) {
+        console.log("number is 0");
+        result = "C'mon; gimme something to work with here...";
+      } else if (number > 3) {
+        console.log("number is more than 3");
+        result = "Too much!";
+      } else if (number <= 3) {
+        console.log("number is 3 or less");
+        result = "Enjoy!"
+      }
+      $scope.lunchCheckResult = result;
+    };
+
+    $scope.clearResult = function () {
+      $scope.lunchCheckResult = "";
+    }
+
+    var checkIt = function() {
+      var items = [];
+      var rawItems = $scope.lunchContents.split(",");
+      for (var i = 0; i < rawItems.length; i++) {
+        var trimmedItem = rawItems[i].toString().trim();
+        if (trimmedItem.length > 0) {
+          items.push(trimmedItem);
+        }
+      }
+      return items.length;
     };
   }
 })();
