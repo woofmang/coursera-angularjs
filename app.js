1090b198f0f2fdfe34463dd8ad6d6b57bab87c7a
woofmang <kevin@woofmang.com>
2017-08-31 21:10:33 -0400

    Lecture 15 example

--- app.js
+++ app.js
@@ -3,39 +3,18 @@
   angular.module('CounterApp', [])
   .controller('CounterController', CounterController);
 
-  CounterController.$inject = ['$scope'];
+  CounterController.$inject = ['$scope','$timeout'];
 
-  function CounterController($scope) {
+  function CounterController($scope, $timeout) {
 
-    $scope.onceCounter = 0;
     $scope.counter = 0;
-    $scope.name = "Kevin";
-
-    $scope.showNumberOfWatchers = function() {
-      console.log("watchers: " + $scope.$$watchersCount);
-    };
-
-    $scope.countOnce = function(){
-      $scope.onceCounter = 1;
-    };
 
     $scope.upCounter = function(){
-      $scope.counter++;
+      $timeout(function(){
+        $scope.counter++;
+        console.log("Counter incremented");
+      }, 2000);
     };
-
-    $scope.$watch(function(){
-      console.log("Digest Loop fired!");
-    });
-
-    // $scope.$watch('onceCounter', function(newValue, oldValue) {
-    //   console.log("onceCounter old value: ", oldValue);
-    //   console.log("onceCounter new value: ", newValue);
-    // });
-    //
-    // $scope.$watch('counter', function(newValue, oldValue) {
-    //   console.log("counter old value: ", oldValue);
-    //   console.log("counter new value: ", newValue);
-    // });
 }
 
 })();
