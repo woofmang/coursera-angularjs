cb3672df290de0ff536233256510ec28df96f54c
woofmang <kevin@woofmang.com>
2017-09-03 10:23:40 -0400

    Lecture 18 examples

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 (function(){
   'use strict';
 
-  var shoppingList1 = [
+  var shoppingList = [
     'Milk', 'Donuts', 'Cookies', 'Chocolate', 'Peanut Butter', 'Pepto Bismol',
     'Pepto Bismol (Chocolate Flavor)', 'Pepto Bismol (Cookie Flavor)'
-  ];
-
-  var shoppingList2 = [
-    {
-      name: 'Milk',
-      quantity: '2'
-    },
-    {
-      name: 'Donuts',
-      quantity: '200'
-    },
-    {
-      name: 'Cookies',
-      quantity: '300'
-    },
-    {
-      name: 'Chocolate',
-      quantity: '5'
-    }
   ];
 
   angular.module('ShoppingListApp', [])
@@ -30,17 +11,7 @@
 
   ShoppingListController.$inject = ['$scope'];
   function ShoppingListController($scope) {
-    $scope.shoppingList1 = shoppingList1;
-    $scope.shoppingList2 = shoppingList2;
-
-    $scope.addToList = function() {
-      var newItem = {
-        name: $scope.newItemName,
-        quantity: $scope.newItemQuantity
-      };
-
-      $scope.shoppingList2.push(newItem);
-    };
+    $scope.shoppingList = shoppingList;
 }
 
 })();
