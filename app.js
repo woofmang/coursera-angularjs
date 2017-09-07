1694ee61ab09ac798bb66708108eab7609ddd4d8
woofmang <kevin@woofmang.com>
2017-09-06 21:57:45 -0400

    Lecture 19, Part 5 examples

--- app.js
+++ app.js
@@ -3,33 +3,45 @@
 
   angular.module('ControllerAsApp', [])
   .controller('ParentController1', ParentController1)
-  .controller('ChildController1', ChildController1);
-  // .controller('ParentController2', ParentController2)
-  // .controller('ChildController2', ChildController2);
+  .controller('ChildController1', ChildController1)
+  .controller('ParentController2', ParentController2)
+  .controller('ChildController2', ChildController2);
 
   ParentController1.$inject = ['$scope'];
   function ParentController1($scope) {
     $scope.parentValue = 1;
     $scope.pc = this;
     $scope.pc.parentValue = 1;
-}
+  }
 
-ChildController1.$inject = ['$scope'];
-function ChildController1($scope) {
-  console.log("$scope.parentValue: ", $scope.parentValue);
-  console.log("CHILD $scope: ", $scope);
+  ChildController1.$inject = ['$scope'];
+  function ChildController1($scope) {
+    // console.log("$scope.parentValue: ", $scope.parentValue);
+    // console.log("CHILD $scope: ", $scope);
+    //
+    // $scope.parentValue = 5;
+    // console.log("*** CHANGED $scope.parentValue to 5 ");
+    // console.log("$scope.parentValue: ", $scope.parentValue);
+    // console.log("CHILD $scope: ", $scope);
+    //
+    // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+    // $scope.pc.parentValue = 5;
+    // console.log("*** CHANGED $scope.pc.parentValue to 5 ");
+    // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+    // console.log("CHILD $scope: ", $scope);
+  }
 
-  $scope.parentValue = 5;
-  console.log("*** CHANGED $scope.parentValue to 5 ");
-  console.log("$scope.parentValue: ", $scope.parentValue);
-  console.log("CHILD $scope: ", $scope);
+  // note: no need to $inject the $scope here
+  function ParentController2($scope) {
+    var parent = this;
+    parent.value = 1;
+  }
 
-  console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-  $scope.pc.parentValue = 5;
-  console.log("*** CHANGED $scope.pc.parentValue to 5 ");
-  console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
-  console.log("CHILD $scope: ", $scope);
-
-}
+  ChildController2.$inject = ['$scope']; // only needed to log the $scope service
+  function ChildController2($scope) {
+    var child = this;
+    child.value = 5;
+    console.log("ChildController2 $scope", $scope);
+  }
 
 })();
