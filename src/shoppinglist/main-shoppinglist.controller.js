d0e0047b1cb972c8c4cd7e5398b26959cd67f3f3
woofmang <kevin@woofmang.com>
2017-10-07 08:36:31 -0400

    Lecture 38 examples

--- src/shoppinglist/main-shoppinglist.controller.js
+++ src/shoppinglist/main-shoppinglist.controller.js
@@ -5,17 +5,19 @@ angular.module('ShoppingList')
 .controller('MainShoppingListController', MainShoppingListController);
 
 
-MainShoppingListController.$inject = ['ShoppingListService'];
-function MainShoppingListController(ShoppingListService) {
+// MainShoppingListController.$inject = ['ShoppingListService'];
+MainShoppingListController.$inject = ['items'];
+// function MainShoppingListController(ShoppingListService) {
+function MainShoppingListController(items) {
   var mainList = this;
-  mainList.items = [];
+  mainList.items = items;
 
-  mainList.$onInit = function () {
-    ShoppingListService.getItems()
-    .then(function (result) {
-      mainList.items = result;
-    });
-  };
+  // mainList.$onInit = function () {
+  //   ShoppingListService.getItems()
+  //   .then(function (result) {
+  //     mainList.items = result;
+  //   });
+  // };
 }
 
 })();
